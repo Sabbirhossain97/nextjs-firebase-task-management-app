03fb4edba8af1a0ccade804fd76949e6518e63b0
Sabbirhossain94 <sabbirshawon1994@hotmail.com>
2023-06-12 14:03:29 +0600

    added Response for handling errors for api/clearAll route

--- src/app/api/clearAll/route.js
+++ src/app/api/clearAll/route.js
@@ -3,9 +3,11 @@ import Todo from "../../../../models/todo";
 export async function GET() {
   try {
     const response = await Todo.deleteMany({})
-    console.log("successfully deleted all documents");
+    console.log("successfully deleted all documents!");
     return new Response(response);
   } catch (err) {
     console.log("error deleting all documents");
+    return new Response("error deleting all documents!");
+
   }
 }
